03a502e5a4a7e17b6e7a77c4b5a8aa0c0a9c85cd
enk-it <gladysh.danil@gmail.com>
2025-04-09 21:24:52 +0700

    refactor: Translate errors

--- src/image_provider/slurp_image_provider.rs
+++ src/image_provider/slurp_image_provider.rs
@@ -9,17 +9,17 @@ impl SlurpImageProvider {
     fn get_region_from_slurp(&self) -> Result<(i32, i32, u32, u32), Box<dyn std::error::Error>> {
         let output = Command::new("slurp")
             .output()
-            .expect("Не удалось запустить slurp. Убедитесь, что он установлен.");
+            .expect("Couldnt start slurp.");
 
         if !output.status.success() {
             let stderr = String::from_utf8_lossy(&output.stderr);
-            return Err(format!("Slurp завершился с ошибкой: {}", stderr).into());
+            return Err(format!("Slurp ended with exception: {}", stderr).into());
         }
 
         let output_str = String::from_utf8(output.stdout)?;
         let parts: Vec<&str> = output_str.trim().split_whitespace().collect();
         if parts.len() != 2 {
-            return Err("Неверный формат вывода slurp".into());
+            return Err("Wrong slurp format".into());
         }
 
         let coords: Vec<&str> = parts[0].split(',').collect();
@@ -36,15 +36,14 @@ impl SlurpImageProvider {
 
 impl super::ImageProvider for SlurpImageProvider {
     fn get_image (&self) -> Result<ImageData, io::Error> {
-        let (x, y, width, height) = self.get_region_from_slurp().expect("Ну ошибка ебать какая то");
+        let (x, y, width, height) = self.get_region_from_slurp().expect("Erorr");
 
         let geometry = format!("{},{} {}x{}", x, y, width, height);
-        // println!("Передаём в grim: grim -g '{}'", geometry);
 
         let output = Command::new("grim")
             .args(&["-g", &geometry, "-"])
             .output()
-            .expect("Не удалось запустить grim. Убедитесь, что он установлен.");
+            .expect("Couldnt start grim.");
 
         let img = image::load_from_memory(&output.stdout).expect("Failed to decode image");
 
